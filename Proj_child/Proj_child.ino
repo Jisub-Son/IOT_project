4be9fcff56bbb84cec5170411541672626e49a83
Ji-sub Son <lttileson@naver.com>
2022-06-18 17:15:47 +0900

    something wrong

--- Proj_child/Proj_child.ino
+++ Proj_child/Proj_child.ino
@@ -1,4 +1,15 @@
 #include <Wire.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ESP8266Webhook.h>
+#include "mqtt_secrets.h"
+
+//#define WIFI_SSID "U+Net9700"
+//#define WIFI_PWD  "HB797@0FE6"
+#define WIFI_SSID "MJU_Wireless"
+#define WIFI_PWD  ""
+#define IFTTT_KEY "TAHf6d1iVRTvH1yfFsaBd"
+#define IFTTT_EVENT "mom_sayed"
 
 int MPU_Addr = 0x68;
 int16_t AcZ;
@@ -6,14 +17,35 @@ int16_t AcZ;
 unsigned long long prev = 0;
 int cur = 0;
 int before = 0;
-int shotgun = 0;
+
+WiFiClient myClient;
+PubSubClient mqttClient;
+Webhook webhook(IFTTT_KEY, IFTTT_EVENT);  // create an object
+
+void cbFunc(const char topic[], byte* data, unsigned int length) 
+{  
+  char str[9];
+  int i;
+  for(i=0; i<=(7<length-1?7:length-1); i++){   // Read data to str
+    str[i] = data[i];
+  }
+  str[i] = 0; 
+
+  int STOPGAME = atoi(str);
+  Serial.printf("callback : %d\r\n", STOPGAME);
+//  !strcmp(str, "STOPGAME")
+  if(STOPGAME == 1){       // if str == TEMP publish temp data
+    Serial.println("revceived STOPGAME..!");
+    webhook.trigger();
+  }
+}
 
 void setup() 
 {
   // Set UART
-  Serial.begin(115200);       // set baud rate
-  delay(1000);
-  Serial.println("Hello");    // print "Hello"
+  Serial.begin(115200);
+  delay(500);
+  Serial.printf("UART OK\r\n");
 
   // Set 12C wake-up
   Wire.begin(4, 5);                 // Set as Master SDA : GPIO4, SCL : GPIO5
@@ -21,21 +53,41 @@ void setup()
   Wire.write(0x6B);                 // Reg. Power Management
   Wire.write(1);                    // wake-up MPU6050
   Wire.endTransmission();
+
+  // Connect WiFi
+  Serial.printf("WiFi connecting");
+  WiFi.begin(WIFI_SSID, WIFI_PWD);              // connect to AP
+  while( !(WiFi.status() == WL_CONNECTED) ){    // wait for connected
+    Serial.printf(".");
+    delay(500);
+  }
+  Serial.printf("\r\nWiFi Connected..!\r\n");
+
+  // Connect MQTT 
+  Serial.println("MQTT Connect...");
+  mqttClient.setClient(myClient);
+  mqttClient.setServer("mqtt3.thingspeak.com",1883);  // Set MQTT server and port
+  mqttClient.setCallback(cbFunc);                     // Set Callback function  
+  int mqttConResult = mqttClient.connect(SECRET_MQTT_CLIENT_ID, SECRET_MQTT_USERNAME, SECRET_MQTT_PASSWORD);
+  mqttClient.subscribe("channels/1738848/subcribe/fields/field2");        // Subscribe topic
+  Serial.printf("MQTT Conn Result : %d\r\n", mqttConResult);
 }
 
 void loop() 
 {
   if(millis() - prev >= 10){
     prev = millis();
-    
+
+    // Read MPU6050 AcZ
     Wire.beginTransmission(MPU_Addr);
     Wire.write(0x3F);           // Reg. ACCEL_XOUT  
     Wire.endTransmission();
-
+    
     Wire.requestFrom(MPU_Addr, 2, true); // Request 2Byte data
     AcZ = Wire.read() << 8 | Wire.read(); // ACCEL_ZOUT
 //    Serial.printf("%d\r\n", AcZ);
 
+    // Set shotgun 1 when gamer smashed the desk
     if(AcZ >= 20000){
       cur = 1;
     }
@@ -44,10 +96,16 @@ void loop()
     }
     
     if(cur == 1 && before == 0){
-      shotgun = 1;
+      int shotgun = 1;
       Serial.printf("SHOTGUN..! : %d\r\n", AcZ);
+
+      // publish to thingspeak server by MQTT publish
+      char str[80];
+      sprintf(str, "%d", shotgun);
+      mqttClient.publish("channels/1738848/publish/fields/field1", str);
+      Serial.printf("send?\r\n");
     }
     before = cur;
-    
   }
+  mqttClient.loop();
 }
