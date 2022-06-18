a3ecaecb2f6a0a8eaec94ae7cdcfd738a52d31c1
max96ssb <41907781+max96ssb@users.noreply.github.com>
2022-06-19 03:39:14 +0900

    get api들 subroutine화 /

--- Proj_parent/Proj_parent.ino
+++ Proj_parent/Proj_parent.ino
@@ -43,6 +43,8 @@ String apiAddr = "/lol/summoner/v4/summoners/by-name/";
 String summoner = "도비준";
 String apiKey = "RGAPI-2b6e0c3f-3416-4563-af8d-7934e9acb8f3";
 
+String line;
+
 //접속 API용 변수들
 String api_login = "/lol/spectator/v4/active-games/by-summoner/";
 //String enc_id = "f0OBDw_rm2OjbLxm6TM-_S_21es6ZEF1jFtAxIfVYwe4hg"; //성범ID
@@ -79,28 +81,8 @@ void fnNotFound(void)
   httpServer.send(404, "text/plain", "WRONG!!!");
 }
 
-void setup()
-{
-  // Set UART
-  Serial.begin(115200);
-  delay(500);
-  Serial.printf("UART OK\r\n");
-
-  //LED 설정
-  pinMode(GRNLED_PORT,OUTPUT);
-  pinMode(REDLED_PORT,OUTPUT);
-  
-  // Connect WiFi
-  Serial.printf("WiFi connecting");
-  WiFi.begin(WIFI_SSID, WIFI_PWD);              // connect to AP
-  while( !(WiFi.status() == WL_CONNECTED) ){    // wait for connected
-    Serial.printf(".");
-    delay(500);
-  }
-  Serial.printf("\r\nWiFi Connected..!\r\n");
-  httpsClient.setInsecure();
-  delay(500);
-  
+//enc_id API
+void get_enc(){
   // https connecting
   if(httpsClient.connect(host, httpsPort)){
     Serial.printf("Connected to web\r\n");
@@ -108,58 +90,71 @@ void setup()
   else{
     Serial.printf("Connection failed\r\n");
   }
+  String link = apiAddr + summoner + "?api_key=" + apiKey;
 
-  // Get local IP
-  Serial.printf("Please contact IP Addr...");
-  Serial.println(WiFi.localIP());
-
-  // Callback functions & start web server
-  httpServer.on("/", fnRoot);
-  httpServer.on("/status", fnStatus);
-  httpServer.on("/sendMsg", fnSendMsg);
-  httpServer.onNotFound(fnNotFound);
-  httpServer.begin();
-  
-  // Connect MQTT 
-  Serial.println("MQTT Connect...");
-  mqttClient.setClient(myClient);
-  mqttClient.setServer("mqtt3.thingspeak.com", 1883);  // Set MQTT server and port
-//  mqttClient.setCallback(cbFunc);                     // Set Callback function  
-  int mqttConResult = mqttClient.connect(SECRET_MQTT_CLIENT_ID_SJS, SECRET_MQTT_USERNAME_SJS, SECRET_MQTT_PASSWORD_SJS);
-//  mqttClient.subscribe("channels/1738848/subscribe/fields/field2");        // Subscribe topic
-  Serial.printf("MQTT Conn Result : %d\r\n", mqttConResult);
-  
-  // Get data
-  String link, link2;
-  link = apiAddr + summoner + "?api_key=" + apiKey;
-  link2 = api_login + enc_id + "?api_key=" + apiKey;
-
-  //enc_id api
-  /*
   httpsClient.print(String("GET ") + link + " HTTP/1.1\r\n" +
                     "Host: " + host + "\r\n" +
                     "Connection: close\r\n\r\n");
-  */
-  //접속상태 api
-  httpsClient.print(String("GET ") + link2 + " HTTP/1.1\r\n" +
-                    "Host: " + host + "\r\n" +
-                    "Connection: close\r\n\r\n");
-  
   Serial.println("request sent");
-  
   //헤더 읽기(표시는 안함)
   while (httpsClient.connected()) {
-    String line = httpsClient.readStringUntil('\n');
+    line = httpsClient.readStringUntil('\n');
     if (line == "\r") {
       Serial.println("headers received");
       break;
     }
   }
-
   //Body 읽기
   Serial.println("reply was:");
   Serial.println("==========");
-  String line;
+  //첫줄 받아서 넘기기
+  line = httpsClient.readStringUntil('\n');  //Read Line by Line
+  Serial.println(line); //Print response
+  //둘째줄 JSON 해체
+  line = httpsClient.readStringUntil('\n');  //Read Line by Line
+  Serial.println(line); //Print response
+  deserializeJson(doc, line.c_str());
+  login_stat = doc["status"]["status_code"];
+  game_id = doc["gameId"];
+  const char *enc_id_c = doc["id"];
+  String enc_id_load(enc_id_c);
+  enc_id = enc_id_load;
+  //나머지 넘기기
+  while(httpsClient.available()){        
+    line = httpsClient.readStringUntil('\n');  //Read Line by Line
+    Serial.println(line); //Print response
+  }
+  Serial.println("==========");
+  Serial.println("closing connection");
+
+  Serial.printf("%s\r\n",enc_id.c_str());
+}
+//접속상태 API
+void get_login(){
+  // https connecting
+  if(httpsClient.connect(host, httpsPort)){
+    Serial.printf("Connected to web\r\n");
+  }
+  else{
+    Serial.printf("Connection failed\r\n");
+  }
+  String link2 = api_login + enc_id + "?api_key=" + apiKey;
+
+  httpsClient.print(String("GET ") + link2 + " HTTP/1.1\r\n" +
+                    "Host: " + host + "\r\n" +
+                    "Connection: close\r\n\r\n");
+  Serial.println("request sent");
+  //헤더 읽기(표시는 안함)
+  while (httpsClient.connected()) {
+    line = httpsClient.readStringUntil('\n');
+    if (line == "\r") {
+      Serial.println("headers received");
+      break;
+    }
+  }
+  //Body 읽기
+  Serial.println("reply was:");
+  Serial.println("==========");
   //첫줄 받아서 넘기기
   line = httpsClient.readStringUntil('\n');  //Read Line by Line
   Serial.println(line); //Print response
@@ -189,6 +184,54 @@ void setup()
   }
   Serial.printf("%d\r\n",login_stat);
   Serial.printf("%lld\r\n",game_id);
+}
+
+
+void setup()
+{
+  // Set UART
+  Serial.begin(115200);
+  delay(500);
+  Serial.printf("UART OK\r\n");
+
+  //LED 설정
+  pinMode(GRNLED_PORT,OUTPUT);
+  pinMode(REDLED_PORT,OUTPUT);
+  
+  // Connect WiFi
+  Serial.printf("WiFi connecting");
+  WiFi.begin(WIFI_SSID, WIFI_PWD);              // connect to AP
+  while( !(WiFi.status() == WL_CONNECTED) ){    // wait for connected
+    Serial.printf(".");
+    delay(500);
+  }
+  Serial.printf("\r\nWiFi Connected..!\r\n");
+  httpsClient.setInsecure();
+  delay(500);
+  
+  // Get local IP
+  Serial.printf("Please contact IP Addr...");
+  Serial.println(WiFi.localIP());
+
+  // Callback functions & start web server
+  httpServer.on("/", fnRoot);
+  httpServer.on("/status", fnStatus);
+  httpServer.on("/sendMsg", fnSendMsg);
+  httpServer.onNotFound(fnNotFound);
+  httpServer.begin();
+  
+  // Connect MQTT 
+  Serial.println("MQTT Connect...");
+  mqttClient.setClient(myClient);
+  mqttClient.setServer("mqtt3.thingspeak.com", 1883);  // Set MQTT server and port
+//  mqttClient.setCallback(cbFunc);                     // Set Callback function  
+  int mqttConResult = mqttClient.connect(SECRET_MQTT_CLIENT_ID_SJS, SECRET_MQTT_USERNAME_SJS, SECRET_MQTT_PASSWORD_SJS);
+//  mqttClient.subscribe("channels/1738848/subscribe/fields/field2");        // Subscribe topic
+  Serial.printf("MQTT Conn Result : %d\r\n", mqttConResult);
+  
+ get_enc();
+ delay(500);
+ get_login();
 }
 
 unsigned long long prev = 0;
